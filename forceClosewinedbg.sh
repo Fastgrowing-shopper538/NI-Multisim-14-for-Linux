3a2c558a1c194fdd3883bd9218a24063c12db876
Lorenzo Pappalardo <p.lorenzo2008@gmail.com>
2026-05-08 21:20:25 +0200

    Update and rename ded.sh to forceClosewinedbg.sh
    removed its meme factor

---
+++ forceClosewinedbg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the prefix path
+PREFIX="$HOME/.multisim32"
+
+echo "Monitoring $PREFIX... Press [CTRL+C] to stop."
+
+while true
+do
+    # Force-kill the wineserver for this prefix
+    killall winedbg > /dev/null 2>&1
+
+    # Wait 1 second before the next check to save CPU cycles
+    sleep 1
+done
